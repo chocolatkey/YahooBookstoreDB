016b9e79eca8efd8ae77e54b5bef4996feb29f58
Henry <chocolatkey@gmail.com>
2017-09-18 14:23:26 -0700

    Update cron

--- cron.sh
+++ cron.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# Henry (chocolatkey) 2017-07-21
+# Henry (chocolatkey) 2017-08-06
 if [ $# -eq 0 ]
   then
     echo "YDB updater"
     echo "Please provide ARGUMENT for additional DB file name"
 else
-    python3.6 /home/media/manga/yrip.py -c true -f true -d true -a "$@"-cron.json -r 798000-810000
+    python3.6 /home/media/manga/yrip.py -c true -f true -d true -a "$@"-cron.json -r 801000-820000
+    sudo chmod 0777 /home/media/manga/db.json
     sudo cp -R /home/media/manga/"$@"-cron.json /var/solr
     sudo su - solr -c "/opt/solr/bin/post -c ydb *-cron.json"
     sleep 1
